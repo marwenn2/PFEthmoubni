89dc3610be9455c2d4ff870771d3dd1fee88f6e3
ziede <ziede.ben.yahya@gmail.com>
2021-06-14 14:31:08 +0100

    loginPage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from "react-router-dom"; 
 import Navigation from './components/Navigation/Navigation';
+import LoginPage  from './pages/login';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isAuth,setIsAuth]= useState(false) ;
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+  },[])  
   return (
     <div className="App">
       <Navigation />
 
       <Switch>
-        <Route exact path='/login' />
+        <Route exact path='/login' component={LoginPage} />
+        <Route exact path ='/register' /> 
       </Switch>
       
     </div>
---
+++ my-app/src/pages/login.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react' ; 
+import styled from 'styled-components' ;
+import TextField from '@material-ui/core/TextField';
+import {Link} from 'react-router-dom'
+import loginSVG from '../assets/login.svg'
+import axios from 'axios' ; 
+
+const SignInButton = styled.button`
+margin-top: 20px ; 
+margin-left: 50px ; 
+background-color: #4b9deb ; 
+border: 0 ; 
+border-radius: 14px ; 
+width: 60% ; 
+color: white ; 
+
+
+padding-top: 10px ; 
+padding-bottom : 10px ; 
+padding-left: 5px ; 
+padding-right: 5px ;
+transition-duration: 0.3s ; 
+&: hover{
+    opacity: 0.8;
+    cursor: pointer;  
+}
+
+
+`
+
+const FlexContainer = styled.div`
+display: flex ; 
+justify-content: space-between ; 
+align-items: center ; 
+
+`
+const ColContainer = styled.div`
+display: flex ; 
+flex-direction:column ;  
+justify-content: center ; 
+`
+const ColWrapper = styled.div`
+margin-right: 18% ;  
+display: flex ; 
+flex-direction:column ;  
+justify-content: center ; 
+`
+const SidePage = styled.div`
+width: 35% ;  
+height: 88vh ; 
+`
+const Flex= styled.div`
+display: flex ; 
+justify-content: space-between ; 
+
+`
+const IMG = styled.img`
+ width: 160% ; 
+ height: 100% ; 
+`
+const Error= styled.div`
+color: red;`
+const LoginPage = ()=>{
+
+    const [email , setEmail ] = useState("") ; 
+    const [password, setPassword] = useState("") ; 
+    const [error,setError]= useState("") ; 
+
+    const handleClick = async()=> {
+        try {
+            const response = await axios.post('/users/login',{email,password})  
+            localStorage.setItem("token",response.data.token) ;   
+        } catch (error) {
+            setError(error.response.data.errors[0].msg) ; 
+            console.log(error.response.data.errors[0].msg) ;  
+        }
+       
+       
+    }
+
+return (
+    <>  
+    <Flex> 
+    <SidePage>
+        <IMG src={loginSVG} ></IMG>
+     </SidePage>
+    <ColWrapper>
+    <Link>Don't have an account ? Register</Link>
+        <h2>Login to Khadamny</h2>
+        <Error>{error}</Error>    
+        
+        <form>
+            <h4>Email adress</h4>
+            <TextField label="Email" color='primary'
+            variant='outlined'
+            type='email'
+            error={Boolean(error)}
+            onChange={(e)=>setEmail(e.target.value)}/>
+            
+
+            <ColContainer>
+            <FlexContainer>
+            <h4>Password</h4>
+            <Link >forgot password ?</Link>
+            </FlexContainer>
+            
+
+            <TextField label="Password" variant='outlined'
+            type='password'
+            onChange={(e)=>setPassword(e.target.value)}/>
+            </ColContainer>
+           
+        </form>
+        <SignInButton onClick={handleClick}>Sign in </SignInButton>
+        </ColWrapper>
+    </Flex>
+    
+       
+    </>
+)
+}
+
+export default LoginPage ; 
